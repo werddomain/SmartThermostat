be14f10d71552678dc2c6c2330b55e07b9561096
Benoit Robin <clefweb@outlook.com>
2018-12-15 00:04:22 -0500

    Implemented float value, a protocol and a send/response mecanism

--- WinIot/ST.WinIot.App/PieArduino/PieArduino.ino
+++ WinIot/ST.WinIot.App/PieArduino/PieArduino.ino
@@ -23,6 +23,12 @@ bool      DataReceived;
 
 float humidity;
 float temperature;
+
+// ***
+// *** Holds the last command and parameter sent by the master.
+uint8_t _lastCommand = 0;
+uint8_t _lastParameter = 0;
+
 void setup() {
 	// put your setup code here, to run once:
 	dht.begin();
@@ -36,25 +42,25 @@ void setup() {
 
 void loop() {
 	// put your main code here, to run repeatedly:
+	if (!DataReceived) {
+		// Reading temperature or humidity takes about 250 milliseconds!
+		// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+		delay(2000);
+		float h = dht.readHumidity();
+		// Read temperature as Celsius (the default)
+		float t = dht.readTemperature();
+		if (isnan(h) || isnan(t)) {
+			Serial.println("Failed to read from DHT sensor!");
+			return;
+		}
+		humidity = h;
+		temperature = t;
+		Serial.print("H: ");
+		Serial.print(h);
+		Serial.print(" T: ");
+		Serial.println(t);
 
-	// Reading temperature or humidity takes about 250 milliseconds!
-	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-	delay(2000);
-	float h = dht.readHumidity();
-	// Read temperature as Celsius (the default)
-	float t = dht.readTemperature();
-	if (isnan(h) || isnan(t)) {
-		Serial.println("Failed to read from DHT sensor!");
-		return;
 	}
-	humidity = h;
-	temperature = t;
-	Serial.print("H: ");
-	Serial.print(h);
-	Serial.print(" T: ");
-	Serial.println(t);
-
-
 	//  if (DataReceived)
 	//  {
 	//    if (ReceivedData[0] = 1)
@@ -65,23 +71,125 @@ void loop() {
 	//  }
 
 }
-
+void clearResponse()
+{
+	memset(Response, 0, sizeof(Response));
+	// ***
+  // *** Set all the bytes to 0
+  // ***
+	Serial.print("Clearing Response :");
+	for (int i = 0; i < sizeof(Response); i++)
+	{
+		Response[i] = 0;
+		Serial.print(i);
+	}
+	Serial.println(" Done");
+}
 void receiveData(int numOfBytesReceived)
 {
-	int indexer = 0;
-	while (Wire.available())
+	// ***
+ // *** We are expecting 2 bytes from the master. The first byte is the command.
+ // *** the second byte is the command parameter.
+ // ***
+ // *** Commands:
+ // ***
+ // *** 1 => GetData.
+ // *** 2 => SetData.
+ // ***
+ // *** Parameters:
+ // *** 10 => Temperature
+ // *** 11 => Humidity
+	Serial.print("Received Data: Size:");
+	Serial.print(numOfBytesReceived);
+	Serial.print(" ");
+
+	if (numOfBytesReceived == 2)
 	{
-		ReceivedData[indexer] = Wire.read();
-		indexer++;
+		// ***
+		// *** Store the command and parameter.
+		// ***
+		_lastCommand = Wire.read();
+		_lastParameter = Wire.read();
+		Serial.print("Command:");
+		Serial.print(_lastCommand);
+		Serial.print(" Param:");
+		Serial.println(_lastParameter);
+		DataReceived = true;
 	}
-	DataReceived = true;
+	else
+	{
+		// ***
+		// *** Clear the receive buffer.
+		// ***
+		while (Wire.available())
+		{
+			Wire.read();
+		}
+
+		// ***
+		// *** Reset
+		// ***
+		_lastCommand = 0;
+		_lastParameter = 0;
+	}
+}
+
+typedef union
+{
+	float number;
+	uint8_t bytes[4];
+} FLOATUNION_t;
+
+const byte ID_Temperature = 10;
+const byte ID_Humidity = 11;
+
+const byte  Type_Float = 100;
+
+void Pack(byte identifier, float value, byte data[])
+{
+	FLOATUNION_t u;
+	u.number = value;
+	data[0] = (byte)1;
+	data[1] = identifier;
+	data[2] = Type_Float;
+	data[3] = u.bytes[0];
+	data[4] = u.bytes[1];
+	data[5] = u.bytes[2];
+	data[6] = u.bytes[3];
+
 }
 
 void sendData()
 {
-	Response[0] = (byte)humidity;
-	Response[1] = (byte)temperature;
-	Response[2] = (byte)0;
+	/*FLOATUNION_t Hum;
+	FLOATUNION_t Temp;
 
+	Hum.number = humidity;
+	Temp.number = temperature;*/
+
+	/*Response[0] = (byte)humidity;
+	Response[1] = (byte)temperature;*/
+
+	clearResponse();
+	bool hit = false;
+	if (_lastCommand == 1)
+		switch (_lastParameter)
+		{
+		case ID_Humidity:
+			Pack(ID_Humidity, humidity, Response);
+			hit = true;
+			break;
+		case ID_Temperature:
+			Pack(ID_Temperature, temperature, Response);
+			hit = true;
+			break;
+		}
+	if (!hit) {
+		Response[0] = 40;
+		Response[1] = 4;
+		Response[3] = _lastCommand;
+		Response[4] = _lastParameter;
+	}
 	Wire.write(Response, 14);
+	DataReceived = false;
 }
--- WinIot/ST.WinIot.App/ST.WinIot.App/Sensors/Arduino.cs
+++ WinIot/ST.WinIot.App/ST.WinIot.App/Sensors/Arduino.cs
@@ -24,16 +24,16 @@ namespace ST.WinIot.App.Sensors
             }
         }
 
-        private string pTemperature;
-        public string Temperature
+        private float pTemperature;
+        public float Temperature
         {
             get { return pTemperature; }
             set { pTemperature = value; RaisePropertyChange("Temperature"); }
         }
 
 
-        private string pHumidity;
-        public string Humidity
+        private float pHumidity;
+        public float Humidity
         {
             get { return pHumidity; }
             set { pHumidity = value; RaisePropertyChange("Humidity"); }
@@ -55,38 +55,71 @@ namespace ST.WinIot.App.Sensors
             _device = await I2cDevice.FromIdAsync(dis[0].Id, settings);
             _periodicTimer = new Timer(TimerCallback, null, 0, 1000); // Create a timer
         }
+        private float GetFLOATUNION(byte[] r) {
+            return ToFloat(new byte[] {r[3], r[4], r[5], r[6] });
+        }
+        private string GetBytes(byte[] r) {
+            var i = r.Select(o => (int)o);
+            return String.Join("-", i);
 
+        }
+        private async Task ProceadResponse(byte[] response) {
+            await Dispatcher.RunAsync(
+                    Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        System.Diagnostics.Debug.WriteLine("Procead Response: " + GetBytes(response));
+                        if (response.Length == 14 && response[0] == 1)
+                        {
+                            switch (response[1])
+                            {
+                                case (byte)COMMANDS.Temperature: //Temperature
+                                    Temperature = GetFLOATUNION(response);
+                                    break;
+                                case (byte)COMMANDS.Humidity: //Humidity
+                                    Humidity = GetFLOATUNION(response);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+
+                    });
+            
+        }
+        enum COMMANDS {
+            Temperature = 10,
+            Humidity = 11
+        }
+       
+
+        const byte Type_Float = 100;
+
+        private Task Get(COMMANDS command) {
+            return Task.Run(async () => {
+                byte[] ReadBuf = new byte[14];
+
+                _device.WriteRead(new byte[] { 1, (byte)command }, ReadBuf);
+                await ProceadResponse(ReadBuf);
+                await Task.Delay(250);
+            });
+
+
+        }
         private async void TimerCallback(object state)
         {
             try
             {
-                byte[] ReadBuf = new byte[14];
-                
-                _device.Read(ReadBuf);
+
+
                 //Float communication in i2c : https://www.raspberrypi.org/forums/viewtopic.php?t=191208
                 //Parse 4 byte to a float : https://stackoverflow.com/questions/4301623/problem-converting-4-bytes-array-to-float-in-c-sharp
-                var humid = (int)ReadBuf[0];
-                var temp = (int)ReadBuf[1];
-                //var soil = (float)ReadBuf[2];
-
-                //if (temp < 30 && humid < 50 && soil < 50)
-                //{
-                //    _device.Write(new byte[] { I2cConstants.SET_PINSTATE, 3, I2cConstants.PINSTATE_HIGH });
-                //}
-                //else
-                //{
-                //    _device.Write(new byte[] { I2cConstants.SET_PINSTATE, 3, I2cConstants.PINSTATE_LOW });
-                //}
+                
+                await Get(COMMANDS.Temperature);
+                await Get(COMMANDS.Humidity);
 
 
 
-                await Dispatcher.RunAsync(
-                    Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-                    {
 
-                        Temperature = temp.ToString();
-                        Humidity = humid.ToString();
-                    });
 
             }
             catch (Exception f)
@@ -95,6 +128,12 @@ namespace ST.WinIot.App.Sensors
             }
 
         }
+        static float ToFloat(byte[] input)
+        {
+            
+            return BitConverter.ToSingle(input, 0);
+        }
+
 
     }
 }
