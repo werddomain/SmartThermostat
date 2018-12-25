e9bedf90ab32b0891f21f0fa1bd198cee9ff4016
Benoit Robin <werddomain@gmail.com>
2018-12-25 02:18:12 -0500

    Added Config project

---
+++ WinIot/ST.WinIot.App/ST.WinIot.App.Web.Config/Urls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ST.WinIot.App.Web.Config
+{
+    public class Urls
+    {
+        public const string Host = "dev.kazo.ca";
+        public const string ApiUrl = Host + "/API";
+        public const string UiUrl = Host;
+        public const string OAuthUrl = Host + "/Auth";
+
+    }
+}
